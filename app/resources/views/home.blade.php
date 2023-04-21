871b9efdd28f8340d5360a0e32d2539b6e19ec5b
高橋柚衣 <yuchan@takahashiyuzuinoMacBook-Pro.local>
2023-04-22 05:32:28 +0900

    初プッシュ

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -91,21 +91,30 @@
 
 
 
-
-<section>
-  <h1>Classy Footer</h1>
-  <h3>Hover over the circle below</h3>
+<footer class="flex-rw">
+<section class="footer-social-section flex-rw">
+      <span class="footer-social-overlap footer-social-connect">
+      CONNECT <span class="footer-social-small">with</span> US
+      </span>
+      <span class="footer-social-overlap footer-social-icons-wrapper">
+      <a href="https://www.pinterest.com/paviliongift/" class="generic-anchor" target="_blank" title="Pinterest" itemprop="significantLink"><i class="fa fa-pinterest"></i></a>
+      <a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fa fa-twitter"></i></a>
+      <a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fa fa-instagram"></i></a>
+      <a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fa fa-youtube"></i></a>
+      <a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
+      </span>
 </section>
-<div class="footer">
-  <div id="button"></div>
-<div id="container">
-<div id="cont">
-<div class="footer_center">
-     <h3>Classy footer text</h3>
-</div>
-</div>
-</div>
-</div>
+      <section class="footer-bottom-section flex-rw">
+      <div class="footer-bottom-wrapper">   
+      <i class="fa fa-copyright" role="copyright"></i> 
+      2019 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
+     </div>
+      <div class="footer-bottom-wrapper">
+      <a href="/terms-of-use.html" class="generic-anchor" rel="nofollow">Terms</a> | <a href="/privacy-policy.html" class="generic-anchor" rel="nofollow">Privacy</a>
+      </div>
+      </section>
+</footer>
 
 
 @endcan
--- app/resources/views/product_detail.blade.php
+++ app/resources/views/product_detail.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 
+
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+
+<div id="overlay">
+    <div id="title"><h1>R'oseate</h1></div>
+
+    </video>
+    <div id="state" class="visible-desktop"><span class="fa fa-pause"></span></div>
+  
+</div>
+<div id="content">
+
 <div class="d-flex justify-content-center">
     <h4 class="gradation02 mb-5">商品詳細</h4>
 </div>
@@ -35,7 +48,30 @@
  </div>
 </div>
 
-
+<footer class="flex-rw">
+<section class="footer-social-section flex-rw">
+      <span class="footer-social-overlap footer-social-connect">
+      CONNECT <span class="footer-social-small">with</span> US
+      </span>
+      <span class="footer-social-overlap footer-social-icons-wrapper">
+      <a href="https://www.pinterest.com/paviliongift/" class="generic-anchor" target="_blank" title="Pinterest" itemprop="significantLink"><i class="fa fa-pinterest"></i></a>
+      <a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><i class="fa fa-facebook"></i></a>
+      <a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><i class="fa fa-twitter"></i></a>
+      <a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><i class="fa fa-instagram"></i></a>
+      <a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><i class="fa fa-youtube"></i></a>
+      <a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a>
+      </span>
+</section>
+      <section class="footer-bottom-section flex-rw">
+      <div class="footer-bottom-wrapper">   
+      <i class="fa fa-copyright" role="copyright"></i> 
+      2019 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
+     </div>
+      <div class="footer-bottom-wrapper">
+      <a href="/terms-of-use.html" class="generic-anchor" rel="nofollow">Terms</a> | <a href="/privacy-policy.html" class="generic-anchor" rel="nofollow">Privacy</a>
+      </div>
+      </section>
+</footer>
 
 
 @endsection
